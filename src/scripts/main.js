fd1d3efa7b6275b52e2c9b79e6608e65d49e03f4
Travis Milner <travismilner1@gmail.com>
2020-10-09 14:51:38 -0500

    added functions that return objects to each seed.js and console logged them to main.js to make sure they work. working well

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,29 @@
 console.log("Welcome to the main module")
 import { createPlan } from "./plan.js"
+import { createAsparagus } from "./seeds/asparagus.js"
+import { createCorn } from "./seeds/corn.js"
+import { createPotato } from "./seeds/potato.js"
+import { createSoybean } from "./seeds/soybean.js"
+import { createSunflower } from "./seeds/sunflower.js"
+import { createWheat } from "./seeds/wheat.js"
 
-const yearlyPlan = createPlan();
-console.log(yearlyPlan);
+// const yearlyPlan = createPlan();
+// console.log(yearlyPlan);
+
+const asparagusSeed = createAsparagus()
+console.log(asparagusSeed)
+
+const cornSeed = createCorn()
+console.log(cornSeed)
+
+const potatoSeed = createPotato()
+console.log(potatoSeed)
+
+const soybean = createSoybean()
+console.log(soybean)
+
+const sunflower = createSunflower()
+console.log(sunflower)
+
+const wheat = createWheat()
+console.log(wheat)
