7d89a4f0af7fcd865163a347ef54230c6a59fe31
Travis Milner <travismilner1@gmail.com>
2020-10-11 15:09:01 -0500

    added tractor.js. also tried foreach method but not working. will circle back

--- src/scripts/field.js
+++ src/scripts/field.js
@@ -1,9 +1,19 @@
 let fieldArray = [];
 
 export const addPlant = (seedObj) => {
-    fieldArray.push(seedObj)
+    if (Array.isArray(seedObj) === true) {
+        seedObj.forEach(element => {
+            fieldArray.push(element)
+            
+        });
+        return addPlant;
+
+
+    }
+   
 };
 
 export const usePlants = () => {
+    console.log(fieldArray)
     return fieldArray.slice();
 }
